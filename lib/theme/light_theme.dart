11f4c27d84728ab1ddc7f8604a24935f7faf9f24
ShivamPande18 <shivshivampande18@gmail.com>
2025-03-24 08:32:42 +0530

    Swifly dark theme added

--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:sixam_mart/util/app_constants.dart';
 
-ThemeData dark({Color color = const Color(0xFF54b46b)}) => ThemeData(
+ThemeData dark({Color color = const Color.fromARGB(255, 71, 74, 255)}) =>
+    ThemeData(
       fontFamily: AppConstants.fontFamily,
       primaryColor: color,
-      secondaryHeaderColor: const Color(0xFF009f67),
+      secondaryHeaderColor: const Color(0xFF0077B6),
       disabledColor: const Color(0xffa2a7ad),
       brightness: Brightness.dark,
       hintColor: const Color(0xFFbebebe),
